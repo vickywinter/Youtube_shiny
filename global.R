a9b5c67bb717073459b6d197eb72d6e484674895
vickywinter <32727433+vickywinter@users.noreply.github.com>
2018-07-30 21:25:32 -0700

    Add files via upload

--- global.R
+++ global.R
@@ -30,43 +30,50 @@ library(lubridate,warn.conflicts = FALSE)
 #
 
 
-USA<-read.csv("./Data/USvideos.csv",stringsAsFactors = FALSE)
-Germ<-read.csv("./Data/DEvideos.csv",stringsAsFactors = FALSE)
-Fran<-read.csv("./Data/FRvideos.csv",stringsAsFactors = FALSE)
-GB<-read.csv("./Data/GBvideos.csv",stringsAsFactors = FALSE)
-Cana<-read.csv("./Data/CAvideos.csv",stringsAsFactors = FALSE)
-# #
-USA_Cate<-read.csv("./Data/US_category_id.csv",stringsAsFactors = FALSE)
-Germ_Cate<-read.csv("./Data/DE_category_id.csv",stringsAsFactors = FALSE)
-Fran_Cate<-read.csv("./Data/FR_category_id.csv",stringsAsFactors = FALSE)
-GB_Cate<-read.csv("./Data/GB_category_id.csv",stringsAsFactors = FALSE)
-Cana_Cate<-read.csv("./Data/CA_category_id.csv",stringsAsFactors = FALSE)
+# USA<-read.csv("./Data/USvideos.csv",stringsAsFactors = FALSE)
+# Germ<-read.csv("./Data/DEvideos.csv",stringsAsFactors = FALSE)
+# Fran<-read.csv("./Data/FRvideos.csv",stringsAsFactors = FALSE)
+# GB<-read.csv("./Data/GBvideos.csv",stringsAsFactors = FALSE)
+# Cana<-read.csv("./Data/CAvideos.csv",stringsAsFactors = FALSE)
+# # #
+# USA_Cate<-read.csv("./Data/US_category_id.csv",stringsAsFactors = FALSE)
+# Germ_Cate<-read.csv("./Data/DE_category_id.csv",stringsAsFactors = FALSE)
+# Fran_Cate<-read.csv("./Data/FR_category_id.csv",stringsAsFactors = FALSE)
+# GB_Cate<-read.csv("./Data/GB_category_id.csv",stringsAsFactors = FALSE)
+# Cana_Cate<-read.csv("./Data/CA_category_id.csv",stringsAsFactors = FALSE)
+# 
+# # #
+# # #
+# USA_all<- merge(USA,USA_Cate,by.x='category_id',by.y='id', all.x=TRUE)
+# Germ_all<- merge(Germ,Germ_Cate,by.x='category_id',by.y='id', all.x=TRUE)
+# Fran_all<- merge(Fran,Fran_Cate,by.x='category_id',by.y='id', all.x=TRUE)
+# GB_all<- merge(GB,GB_Cate,by.x='category_id',by.y='id', all.x=TRUE)
+# Cana_all<- merge(Cana,Cana_Cate,by.x='category_id',by.y='id', all.x=TRUE)
+# # #
+# Youtube_all<-rbind(USA_all,Germ_all,Fran_all,GB_all,Cana_all)
 
-# #
-# #
-USA_all<- merge(USA,USA_Cate,by.x='category_id',by.y='id', all.x=TRUE)
-Germ_all<- merge(Germ,Germ_Cate,by.x='category_id',by.y='id', all.x=TRUE)
-Fran_all<- merge(Fran,Fran_Cate,by.x='category_id',by.y='id', all.x=TRUE)
-GB_all<- merge(GB,GB_Cate,by.x='category_id',by.y='id', all.x=TRUE)
-Cana_all<- merge(Cana,Cana_Cate,by.x='category_id',by.y='id', all.x=TRUE)
-# #
-Youtube_all<-rbind(USA_all,Germ_all,Fran_all,GB_all,Cana_all)
+Youtube_all<-read.csv("./Data/Youtube_all.csv",stringsAsFactors = FALSE)
 
-#Youtube_all<-read.csv("./Data//Youtube_all.csv",stringsAsFactors = FALSE)
-
-Youtube_all$publish_time <-substring(Youtube_all$publish_time,1,10)
 Youtube_all$publish_time <-as.Date(Youtube_all$publish_time,"%Y-%m-%d")
 Youtube_all$month<-format(Youtube_all$publish_time, "%Y-%m")
 Youtube_all$Year<-format(Youtube_all$publish_time, "%Y")
-# #
-Youtube_all$category<-ifelse(Youtube_all$category_id==29,"Nonprofits & Activism",Youtube_all$category)
-Youtube_all<-Youtube_all[,c('category','Country','video_id','publish_time','month','Year','channel_title','tags','views','likes','dislikes','comment_count')]
-# #
-Youtube_all<- as.data.frame(group_by(Youtube_all,category,Country,video_id,publish_time,month,Year,channel_title,tags) %>%
-                              summarize(views=max(views),likes=max(likes),dislikes=max(dislikes),comment_count=max(comment_count)))
-
-
 Youtube_all$half_month <- floor_date(Youtube_all$publish_time, unit = "week")
 Youtube_all$weekday<-weekdays(Youtube_all$publish_time)
+
+
+# Youtube_all$publish_time <-substring(Youtube_all$publish_time,1,10)
+# Youtube_all$publish_time <-as.Date(Youtube_all$publish_time,"%Y-%m-%d")
+# Youtube_all$month<-format(Youtube_all$publish_time, "%Y-%m")
+# Youtube_all$Year<-format(Youtube_all$publish_time, "%Y")
+# # #
+# Youtube_all$category<-ifelse(Youtube_all$category_id==29,"Nonprofits & Activism",Youtube_all$category)
+# Youtube_all<-Youtube_all[,c('category','Country','video_id','publish_time','month','Year','channel_title','tags','views','likes','dislikes','comment_count')]
+# # #
+# Youtube_all<- as.data.frame(group_by(Youtube_all,category,Country,video_id,publish_time,month,Year,channel_title,tags) %>%
+#                               summarize(views=max(views),likes=max(likes),dislikes=max(dislikes),comment_count=max(comment_count)))
+# 
+# 
+# Youtube_all$half_month <- floor_date(Youtube_all$publish_time, unit = "week")
+# Youtube_all$weekday<-weekdays(Youtube_all$publish_time)
 
 
